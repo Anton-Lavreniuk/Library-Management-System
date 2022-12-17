238df478c87f9c4a6fdf77b4312df328d55f65d5
Anton-Lavreniuk <lvrnk.a.o@gmail.com>
2022-12-17 21:44:12 +0200

    Added the user menu, and the option to view all books.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import net.proteanit.sql.DbUtils;
+
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.*;
+import java.util.ArrayList;
 import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
@@ -52,9 +56,9 @@ public class Main {
                                 String admin = rs.getString("ADMIN");
                                 String UID=rs.getString("UID");
                                 if(admin.equals("1")){
-                                    //admin_menu();
+                                    admin_menu();
                                 }else {
-                                    //user_menu(UID);
+                                    user_menu(UID);
                                 }
                             }
                         }
@@ -92,5 +96,76 @@ public class Main {
         }
         System.out.println(connection);
         return connection;
+    }
+
+    public static void user_menu(String UID){
+        JFrame f=new JFrame("User functions");
+        JButton view_but = new JButton("View books");
+        view_but.setBounds(20,20,120,25);
+        view_but.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame f = new JFrame("Books available");
+                Connection connection = connect();
+                String sql = "SELECT * FROM BOOKS";
+                try{
+                    Statement stmt = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+                    stmt.executeUpdate("USE MYDB");
+                    stmt=connection.createStatement();
+                    ResultSet rs=stmt.executeQuery(sql);
+                    JTable book_list = new JTable();
+                    book_list.setModel(DbUtils.resultSetToTableModel(rs));
+                    JScrollPane scrollPane = new JScrollPane(book_list);
+
+                    f.add(scrollPane);
+                    f.setSize(800,400);
+                    f.setVisible(true);
+                    f.setLocationRelativeTo(null);
+                }
+                catch (SQLException exx) {
+                    JOptionPane.showMessageDialog(null,exx);
+                }
+            }
+        });
+        JButton my_book= new JButton("My books");
+        my_book.setBounds(150,20,120,25);
+        my_book.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame f = new JFrame("My books");
+                int UID_int = Integer.parseInt(UID);
+
+                Connection connection = connect();
+                String sql = "SELECT DISTINCT MYDB.ISSUED.*,BOOKS.BNAME,BOOKS.GENRE, BOOKS.PRICE FROM MYDB.ISSUED, MYDB.BOOKS WHERE ((MYDB.ISSUED.UID="+UID_int+") and (MYDB.BOOKS.BID in (SELECT BID FROM MYDB.ISSUED WHERE MYDB.ISSUED.UID="+UID_int+"))) GROUP BY IID,BNAME";
+                String sql2= "SELECT BID FROM ISSUED WHERE UID = "+UID_int;
+                try{
+                    Statement stmt = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+                    stmt.executeUpdate("USE MYDB");
+                    stmt=connection.createStatement();
+                    ArrayList books_list = new ArrayList();
+                    ResultSet rs = stmt.executeQuery(sql);
+                    JTable book_list = new JTable();
+                    book_list.setModel(DbUtils.resultSetToTableModel(rs));
+                    JScrollPane scrollPane = new JScrollPane(book_list);
+                    f.add(scrollPane);
+                    f.setSize(800,400);
+                    f.setVisible(true);
+                    f.setLocationRelativeTo(null);
+
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(null,ex);
+                }
+
+            }
+        });
+        f.add(my_book);
+        f.add(view_but);
+        f.setSize(300,100);
+        f.setVisible(true);
+        f.setLocationRelativeTo(null);
+    }
+
+    public static void admin_menu(){
+
     }
 }
